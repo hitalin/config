cdb75bfb45826b57f208083841ce2032ec2ca759
hitalin <hitalin@users.noreply.github.com>
2022-02-14 14:00:43 +0900

    delete unnecessary setting for colorscheme in neovim

--- set.sh
+++ set.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 if [ -e $(which nvim) ] && [ ! -e ~/.config/nvim/ ]; then
   pip3 install --user pynvim neovim-remote
-  ln -Fis $HOME/.cache/nvim/dein/repos/github.com/whatyouhide/vim-gotham/colors $HOME/.config/nvim/colors
 fi
 
 echo "Finished!"
