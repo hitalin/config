7a6b648b8645218dd385104e4fa3bc63b8897e42
hitalin <hitalin@users.noreply.github.com>
2023-11-12 06:01:56 +0900

    Install keepassxc

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,7 +29,8 @@
     pkgs.ripgrep
     pkgs.delta
     pkgs.vscode
- 
+    pkgs.keepassxc
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
