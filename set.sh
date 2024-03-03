24763506b05f5406e2c917d50be29108cc2117de
hitalin <hitalin@users.noreply.github.com>
2024-03-03 15:25:00 +0900

    Replace which command with command -v to comply with POSIX standard

--- set.sh
+++ set.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ -e $(which nvim) ] && [ ! -e ~/.config/nvim/ ]; then
+if command -v nvim >/dev/null 2>&1 && [ ! -e ~/.config/nvim/ ]; then
   pip3 install --user pynvim neovim-remote
 fi
 
