eef0040b59af250da354ea373e51474b2a886369
hitalin <hitalin@proton.me>
2026-06-23 17:47:18 +0900

    chore: add nil nix language server

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -21,6 +21,7 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    pkgs.nil
     pkgs.gh
     pkgs.act
     pkgs.rbw
